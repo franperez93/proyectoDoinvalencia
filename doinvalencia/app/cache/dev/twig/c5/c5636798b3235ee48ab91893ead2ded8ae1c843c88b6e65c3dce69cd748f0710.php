0baf44c4111d37b3e57eab65181c44c9929a1daf
Francisco Perez <frankiko.8@gmail.com>
2016-01-21 08:11:47 +0000

    retoques del home

--- doinvalencia/app/cache/dev/twig/c5/c5636798b3235ee48ab91893ead2ded8ae1c843c88b6e65c3dce69cd748f0710.php
+++ doinvalencia/app/cache/dev/twig/c5/c5636798b3235ee48ab91893ead2ded8ae1c843c88b6e65c3dce69cd748f0710.php
@@ -24,46 +24,46 @@ class __TwigTemplate_86cc6396635569795a638bed915f335151169964189c408d09fc3edd17c
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_ed3cfd11145bf147718c12366e843c9f264f009e8573aeef06c7ca3bbe96eb77 = $this->env->getExtension("native_profiler");
-        $__internal_ed3cfd11145bf147718c12366e843c9f264f009e8573aeef06c7ca3bbe96eb77->enter($__internal_ed3cfd11145bf147718c12366e843c9f264f009e8573aeef06c7ca3bbe96eb77_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "DawplantillaBundle:Default:index.html.twig"));
+        $__internal_f1837b0ff53a8e5f4abea2e11f0144accdf4a504485e08307251a05915b95c71 = $this->env->getExtension("native_profiler");
+        $__internal_f1837b0ff53a8e5f4abea2e11f0144accdf4a504485e08307251a05915b95c71->enter($__internal_f1837b0ff53a8e5f4abea2e11f0144accdf4a504485e08307251a05915b95c71_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "DawplantillaBundle:Default:index.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_ed3cfd11145bf147718c12366e843c9f264f009e8573aeef06c7ca3bbe96eb77->leave($__internal_ed3cfd11145bf147718c12366e843c9f264f009e8573aeef06c7ca3bbe96eb77_prof);
+        $__internal_f1837b0ff53a8e5f4abea2e11f0144accdf4a504485e08307251a05915b95c71->leave($__internal_f1837b0ff53a8e5f4abea2e11f0144accdf4a504485e08307251a05915b95c71_prof);
 
     }
 
     // line 3
     public function block_title($context, array $blocks = array())
     {
-        $__internal_57bf5b2512396762cab237b1ca87025400a5288f663cd3797f817354c3bf0eef = $this->env->getExtension("native_profiler");
-        $__internal_57bf5b2512396762cab237b1ca87025400a5288f663cd3797f817354c3bf0eef->enter($__internal_57bf5b2512396762cab237b1ca87025400a5288f663cd3797f817354c3bf0eef_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+        $__internal_35fbe57d96fb16bcb29b83d2a2adb365751465a498994265e310537f0eb97132 = $this->env->getExtension("native_profiler");
+        $__internal_35fbe57d96fb16bcb29b83d2a2adb365751465a498994265e310537f0eb97132->enter($__internal_35fbe57d96fb16bcb29b83d2a2adb365751465a498994265e310537f0eb97132_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
 
         echo "Pagina principal de DoInValencia";
         
-        $__internal_57bf5b2512396762cab237b1ca87025400a5288f663cd3797f817354c3bf0eef->leave($__internal_57bf5b2512396762cab237b1ca87025400a5288f663cd3797f817354c3bf0eef_prof);
+        $__internal_35fbe57d96fb16bcb29b83d2a2adb365751465a498994265e310537f0eb97132->leave($__internal_35fbe57d96fb16bcb29b83d2a2adb365751465a498994265e310537f0eb97132_prof);
 
     }
 
     // line 4
     public function block_stylesheets($context, array $blocks = array())
     {
-        $__internal_7b12bc9127d5fa540a53cbbe27db5b0d8093be4f47e0af95ac2d3187ff7bfcf6 = $this->env->getExtension("native_profiler");
-        $__internal_7b12bc9127d5fa540a53cbbe27db5b0d8093be4f47e0af95ac2d3187ff7bfcf6->enter($__internal_7b12bc9127d5fa540a53cbbe27db5b0d8093be4f47e0af95ac2d3187ff7bfcf6_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+        $__internal_34431723a04c31fcdcfbdb3d4f0bae6b19cb51877b342bb09074fb679edc7176 = $this->env->getExtension("native_profiler");
+        $__internal_34431723a04c31fcdcfbdb3d4f0bae6b19cb51877b342bb09074fb679edc7176->enter($__internal_34431723a04c31fcdcfbdb3d4f0bae6b19cb51877b342bb09074fb679edc7176_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
 
         echo "<link rel=\"stylesheet\" href=\"";
         echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/css/estilo.css"), "html", null, true);
         echo "\" type=\"text/css\" />";
         
-        $__internal_7b12bc9127d5fa540a53cbbe27db5b0d8093be4f47e0af95ac2d3187ff7bfcf6->leave($__internal_7b12bc9127d5fa540a53cbbe27db5b0d8093be4f47e0af95ac2d3187ff7bfcf6_prof);
+        $__internal_34431723a04c31fcdcfbdb3d4f0bae6b19cb51877b342bb09074fb679edc7176->leave($__internal_34431723a04c31fcdcfbdb3d4f0bae6b19cb51877b342bb09074fb679edc7176_prof);
 
     }
 
     // line 6
     public function block_body($context, array $blocks = array())
     {
-        $__internal_01948398b0c028e50d5f90aac7750cc1219ad8051a630cbd1a810213fb670bc9 = $this->env->getExtension("native_profiler");
-        $__internal_01948398b0c028e50d5f90aac7750cc1219ad8051a630cbd1a810213fb670bc9->enter($__internal_01948398b0c028e50d5f90aac7750cc1219ad8051a630cbd1a810213fb670bc9_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_56a1302d7e6655a70f6613b0b7a3734807992025f5e45443a594bc4793318425 = $this->env->getExtension("native_profiler");
+        $__internal_56a1302d7e6655a70f6613b0b7a3734807992025f5e45443a594bc4793318425->enter($__internal_56a1302d7e6655a70f6613b0b7a3734807992025f5e45443a594bc4793318425_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 7
         echo "
@@ -89,13 +89,13 @@ class __TwigTemplate_86cc6396635569795a638bed915f335151169964189c408d09fc3edd17c
                           <div class=\"thumbnail\">
                             <a href=\"evento\"><img src=\"";
         // line 28
-        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/images/pruebas/mexico-city.png"), "html", null, true);
+        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/images/prueba/mexico-city.png"), "html", null, true);
         echo "\" ></a>
                           </div>
                           <div class=\"thumbnail\">
                             <a href=\"evento\"><img src=\"";
         // line 31
-        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/images/pruebas/ny.png"), "html", null, true);
+        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/images/prueba/ny.png"), "html", null, true);
         echo "\" ></a>
                           </div>
                       </div>
@@ -103,13 +103,13 @@ class __TwigTemplate_86cc6396635569795a638bed915f335151169964189c408d09fc3edd17c
                           <div class=\"thumbnail\">
                             <a href=\"evento\"><img src=\"";
         // line 36
-        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/images/pruebas/tokyo.png"), "html", null, true);
+        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/images/prueba/tokyo.png"), "html", null, true);
         echo "\" ></a>
                           </div>
                           <div class=\"thumbnail\">
                             <a href=\"evento\"><img src=\"";
         // line 39
-        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/images/pruebas/paris.png"), "html", null, true);
+        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/images/prueba/paris.png"), "html", null, true);
         echo "\" ></a>
                           </div>
                       </div>
@@ -142,18 +142,18 @@ class __TwigTemplate_86cc6396635569795a638bed915f335151169964189c408d09fc3edd17c
     </section>
 ";
         
-        $__internal_01948398b0c028e50d5f90aac7750cc1219ad8051a630cbd1a810213fb670bc9->leave($__internal_01948398b0c028e50d5f90aac7750cc1219ad8051a630cbd1a810213fb670bc9_prof);
+        $__internal_56a1302d7e6655a70f6613b0b7a3734807992025f5e45443a594bc4793318425->leave($__internal_56a1302d7e6655a70f6613b0b7a3734807992025f5e45443a594bc4793318425_prof);
 
     }
 
     // line 72
     public function block_javascripts($context, array $blocks = array())
     {
-        $__internal_7c6e880a0b40cf2ccdbab7cc731ebf9e49b70f7f752aa7616caadd7cad028a41 = $this->env->getExtension("native_profiler");
-        $__internal_7c6e880a0b40cf2ccdbab7cc731ebf9e49b70f7f752aa7616caadd7cad028a41->enter($__internal_7c6e880a0b40cf2ccdbab7cc731ebf9e49b70f7f752aa7616caadd7cad028a41_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+        $__internal_172bbbb5af94fce918540d43e59477afe7ab8628bad3beb072015305876590fd = $this->env->getExtension("native_profiler");
+        $__internal_172bbbb5af94fce918540d43e59477afe7ab8628bad3beb072015305876590fd->enter($__internal_172bbbb5af94fce918540d43e59477afe7ab8628bad3beb072015305876590fd_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
 
         
-        $__internal_7c6e880a0b40cf2ccdbab7cc731ebf9e49b70f7f752aa7616caadd7cad028a41->leave($__internal_7c6e880a0b40cf2ccdbab7cc731ebf9e49b70f7f752aa7616caadd7cad028a41_prof);
+        $__internal_172bbbb5af94fce918540d43e59477afe7ab8628bad3beb072015305876590fd->leave($__internal_172bbbb5af94fce918540d43e59477afe7ab8628bad3beb072015305876590fd_prof);
 
     }
 
@@ -199,18 +199,18 @@ class __TwigTemplate_86cc6396635569795a638bed915f335151169964189c408d09fc3edd17c
 /*                     <div class="row">*/
 /*                       <div class="col-md-4">*/
 /*                           <div class="thumbnail">*/
-/*                             <a href="evento"><img src="{{asset('bundles/dawplantilla/images/pruebas/mexico-city.png')}}" ></a>*/
+/*                             <a href="evento"><img src="{{asset('bundles/dawplantilla/images/prueba/mexico-city.png')}}" ></a>*/
 /*                           </div>*/
 /*                           <div class="thumbnail">*/
-/*                             <a href="evento"><img src="{{asset('bundles/dawplantilla/images/pruebas/ny.png')}}" ></a>*/
+/*                             <a href="evento"><img src="{{asset('bundles/dawplantilla/images/prueba/ny.png')}}" ></a>*/
 /*                           </div>*/
 /*                       </div>*/
 /*                       <div class="col-md-4">*/
 /*                           <div class="thumbnail">*/
-/*                             <a href="evento"><img src="{{asset('bundles/dawplantilla/images/pruebas/tokyo.png')}}" ></a>*/
+/*                             <a href="evento"><img src="{{asset('bundles/dawplantilla/images/prueba/tokyo.png')}}" ></a>*/
 /*                           </div>*/
 /*                           <div class="thumbnail">*/
-/*                             <a href="evento"><img src="{{asset('bundles/dawplantilla/images/pruebas/paris.png')}}" ></a>*/
+/*                             <a href="evento"><img src="{{asset('bundles/dawplantilla/images/prueba/paris.png')}}" ></a>*/
 /*                           </div>*/
 /*                       </div>*/
 /*                     </div>*/
--- doinvalencia/app/cache/dev/twig/db/db71ee71334cfdc1f69030c69b38cfeb1734edc22471818278456f372ce6b736.php
+++ doinvalencia/app/cache/dev/twig/db/db71ee71334cfdc1f69030c69b38cfeb1734edc22471818278456f372ce6b736.php
@@ -19,8 +19,8 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_1eeecc3a83b7689e8ce266c2f21db49c5a203343fc5ea57131ed7d42aaba0b63 = $this->env->getExtension("native_profiler");
-        $__internal_1eeecc3a83b7689e8ce266c2f21db49c5a203343fc5ea57131ed7d42aaba0b63->enter($__internal_1eeecc3a83b7689e8ce266c2f21db49c5a203343fc5ea57131ed7d42aaba0b63_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "::base.html.twig"));
+        $__internal_c1794092da10425b9e114ea8984a5573becfdd3f847080a4b7190f11d7116e0e = $this->env->getExtension("native_profiler");
+        $__internal_c1794092da10425b9e114ea8984a5573becfdd3f847080a4b7190f11d7116e0e->enter($__internal_c1794092da10425b9e114ea8984a5573becfdd3f847080a4b7190f11d7116e0e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "::base.html.twig"));
 
         // line 1
         echo "<!DOCTYPE html>
@@ -51,7 +51,7 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
         <section class=\"jumbotron\">
             <div class=\"container\">
                 <h1>DoInValencia</h1>
-                <p>Eventos que podemos encontrar en la provincia de valencia i alrededores</p>
+                <p>Eventos que podemos encontrar en la provincia de valencia y alrededores</p>
             </div>
         </section>
     
@@ -74,7 +74,7 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
                         <li class=\"active\"><a href=\"#\">Inicio</a></li>
                         <li><a href=\"#\">Pagina 1</a></li>
                         <li><a href=\"#\">Pagina 2</a></li>
-                        <li class=\"dropdown\">
+                        <li class=\"dropdown elementos\">
                             <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\">
                             Elementos <span class=\"caret\"></span>
                             </a>
@@ -100,7 +100,7 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
         ";
         // line 66
         $this->displayBlock('body', $context, $blocks);
-        // line 67
+        // line 69
         echo "        
          <!-- Footer de la pagina web-->
         <footer class=\"site-footer\">
@@ -122,59 +122,66 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
     
         <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>
         <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\"></script>
+        <script src=\"";
+        // line 90
+        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("bundles/dawplantilla/js/script.js"), "html", null, true);
+        echo "\"></script>
         ";
-        // line 88
+        // line 91
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 89
+        // line 92
         echo "    </body>
 </html>
 ";
         
-        $__internal_1eeecc3a83b7689e8ce266c2f21db49c5a203343fc5ea57131ed7d42aaba0b63->leave($__internal_1eeecc3a83b7689e8ce266c2f21db49c5a203343fc5ea57131ed7d42aaba0b63_prof);
+        $__internal_c1794092da10425b9e114ea8984a5573becfdd3f847080a4b7190f11d7116e0e->leave($__internal_c1794092da10425b9e114ea8984a5573becfdd3f847080a4b7190f11d7116e0e_prof);
 
     }
 
     // line 5
     public function block_title($context, array $blocks = array())
     {
-        $__internal_d3f85d43a8472390c56f6e16cfcf1abf25fa9cf5eb45f1940b78612694454738 = $this->env->getExtension("native_profiler");
-        $__internal_d3f85d43a8472390c56f6e16cfcf1abf25fa9cf5eb45f1940b78612694454738->enter($__internal_d3f85d43a8472390c56f6e16cfcf1abf25fa9cf5eb45f1940b78612694454738_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+        $__internal_ca211f1538f2dd1af78b5f95c9622bdd8a24fe89d173d5dd5b974ade4cbaa49a = $this->env->getExtension("native_profiler");
+        $__internal_ca211f1538f2dd1af78b5f95c9622bdd8a24fe89d173d5dd5b974ade4cbaa49a->enter($__internal_ca211f1538f2dd1af78b5f95c9622bdd8a24fe89d173d5dd5b974ade4cbaa49a_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
 
         
-        $__internal_d3f85d43a8472390c56f6e16cfcf1abf25fa9cf5eb45f1940b78612694454738->leave($__internal_d3f85d43a8472390c56f6e16cfcf1abf25fa9cf5eb45f1940b78612694454738_prof);
+        $__internal_ca211f1538f2dd1af78b5f95c9622bdd8a24fe89d173d5dd5b974ade4cbaa49a->leave($__internal_ca211f1538f2dd1af78b5f95c9622bdd8a24fe89d173d5dd5b974ade4cbaa49a_prof);
 
     }
 
     // line 10
     public function block_stylesheets($context, array $blocks = array())
     {
-        $__internal_4bc6f707603571dae2b001087521131aaa5f0ad4f133edf4afc99c001f0ee58f = $this->env->getExtension("native_profiler");
-        $__internal_4bc6f707603571dae2b001087521131aaa5f0ad4f133edf4afc99c001f0ee58f->enter($__internal_4bc6f707603571dae2b001087521131aaa5f0ad4f133edf4afc99c001f0ee58f_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+        $__internal_3ec60eee7c9cf3180bda89b590c6351533fd1a0ad0fa333bc6d003b7cf56f5e8 = $this->env->getExtension("native_profiler");
+        $__internal_3ec60eee7c9cf3180bda89b590c6351533fd1a0ad0fa333bc6d003b7cf56f5e8->enter($__internal_3ec60eee7c9cf3180bda89b590c6351533fd1a0ad0fa333bc6d003b7cf56f5e8_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
 
         
-        $__internal_4bc6f707603571dae2b001087521131aaa5f0ad4f133edf4afc99c001f0ee58f->leave($__internal_4bc6f707603571dae2b001087521131aaa5f0ad4f133edf4afc99c001f0ee58f_prof);
+        $__internal_3ec60eee7c9cf3180bda89b590c6351533fd1a0ad0fa333bc6d003b7cf56f5e8->leave($__internal_3ec60eee7c9cf3180bda89b590c6351533fd1a0ad0fa333bc6d003b7cf56f5e8_prof);
 
     }
 
     // line 66
     public function block_body($context, array $blocks = array())
     {
-        $__internal_8da42ceee42d2695225bbc00ba3b82cfb67a72de1e7f3d251a708bdffb8d8f86 = $this->env->getExtension("native_profiler");
-        $__internal_8da42ceee42d2695225bbc00ba3b82cfb67a72de1e7f3d251a708bdffb8d8f86->enter($__internal_8da42ceee42d2695225bbc00ba3b82cfb67a72de1e7f3d251a708bdffb8d8f86_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_a6cefa029720f1292684a16167d95afab1b4e4bd1b5b9c202b6091ad0cc7f445 = $this->env->getExtension("native_profiler");
+        $__internal_a6cefa029720f1292684a16167d95afab1b4e4bd1b5b9c202b6091ad0cc7f445->enter($__internal_a6cefa029720f1292684a16167d95afab1b4e4bd1b5b9c202b6091ad0cc7f445_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
+        // line 67
+        echo "        
+        ";
         
-        $__internal_8da42ceee42d2695225bbc00ba3b82cfb67a72de1e7f3d251a708bdffb8d8f86->leave($__internal_8da42ceee42d2695225bbc00ba3b82cfb67a72de1e7f3d251a708bdffb8d8f86_prof);
+        $__internal_a6cefa029720f1292684a16167d95afab1b4e4bd1b5b9c202b6091ad0cc7f445->leave($__internal_a6cefa029720f1292684a16167d95afab1b4e4bd1b5b9c202b6091ad0cc7f445_prof);
 
     }
 
-    // line 88
+    // line 91
     public function block_javascripts($context, array $blocks = array())
     {
-        $__internal_6e6187ec986832f811fe61e9bfccadd294828ff391d9c81d67fe43ad958ec50e = $this->env->getExtension("native_profiler");
-        $__internal_6e6187ec986832f811fe61e9bfccadd294828ff391d9c81d67fe43ad958ec50e->enter($__internal_6e6187ec986832f811fe61e9bfccadd294828ff391d9c81d67fe43ad958ec50e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+        $__internal_14aa059af9d075a3dfa5c7e581eb446cae03ebf643b1f13ef26715a459771d5b = $this->env->getExtension("native_profiler");
+        $__internal_14aa059af9d075a3dfa5c7e581eb446cae03ebf643b1f13ef26715a459771d5b->enter($__internal_14aa059af9d075a3dfa5c7e581eb446cae03ebf643b1f13ef26715a459771d5b_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
 
         
-        $__internal_6e6187ec986832f811fe61e9bfccadd294828ff391d9c81d67fe43ad958ec50e->leave($__internal_6e6187ec986832f811fe61e9bfccadd294828ff391d9c81d67fe43ad958ec50e_prof);
+        $__internal_14aa059af9d075a3dfa5c7e581eb446cae03ebf643b1f13ef26715a459771d5b->leave($__internal_14aa059af9d075a3dfa5c7e581eb446cae03ebf643b1f13ef26715a459771d5b_prof);
 
     }
 
@@ -190,7 +197,7 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
 
     public function getDebugInfo()
     {
-        return array (  171 => 88,  160 => 66,  149 => 10,  138 => 5,  129 => 89,  127 => 88,  104 => 67,  102 => 66,  45 => 12,  42 => 11,  40 => 10,  32 => 5,  26 => 1,);
+        return array (  178 => 91,  170 => 67,  164 => 66,  153 => 10,  142 => 5,  133 => 92,  131 => 91,  127 => 90,  104 => 69,  102 => 66,  45 => 12,  42 => 11,  40 => 10,  32 => 5,  26 => 1,);
     }
 }
 /* <!DOCTYPE html>*/
@@ -212,7 +219,7 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
 /*         <section class="jumbotron">*/
 /*             <div class="container">*/
 /*                 <h1>DoInValencia</h1>*/
-/*                 <p>Eventos que podemos encontrar en la provincia de valencia i alrededores</p>*/
+/*                 <p>Eventos que podemos encontrar en la provincia de valencia y alrededores</p>*/
 /*             </div>*/
 /*         </section>*/
 /*     */
@@ -235,7 +242,7 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
 /*                         <li class="active"><a href="#">Inicio</a></li>*/
 /*                         <li><a href="#">Pagina 1</a></li>*/
 /*                         <li><a href="#">Pagina 2</a></li>*/
-/*                         <li class="dropdown">*/
+/*                         <li class="dropdown elementos">*/
 /*                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">*/
 /*                             Elementos <span class="caret"></span>*/
 /*                             </a>*/
@@ -258,7 +265,9 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
 /*         </nav>    */
 /*     </header>*/
 /*     */
-/*         {% block body %}{% endblock %}*/
+/*         {% block body %}*/
+/*         */
+/*         {% endblock %}*/
 /*         */
 /*          <!-- Footer de la pagina web-->*/
 /*         <footer class="site-footer">*/
@@ -280,6 +289,7 @@ class __TwigTemplate_c885e46e9d41d43b9948ac562e4b08eab71c7d15d9b1155bd2ca2f4864f
 /*     */
 /*         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>*/
 /*         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>*/
+/*         <script src="{{asset('bundles/dawplantilla/js/script.js')}}"></script>*/
 /*         {% block javascripts %}{% endblock %}*/
 /*     </body>*/
 /* </html>*/
