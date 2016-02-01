e4a3b4450ee4c871e9895169867041f810ab5208
xavi <xavi@geekshubs.com>
2016-02-01 18:34:18 +0100

    Inclusión del sistema de control de usuarios

--- doinvalencia/src/BackEnd/DataBaseBundle/Entity/User.php
+++ doinvalencia/src/BackEnd/DataBaseBundle/Entity/User.php
@@ -2,27 +2,25 @@
 
 namespace BackEnd\DataBaseBundle\Entity;
 
-use FOS\UserBundle\Entiy\User as BaseUser;
+use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
   * @ORM\Entity
   * @ORM\Table(name="fos_user")
   */
-
 class User extends BaseUser
 {
 /**
   * @ORM\Id
-  * @ORM\Comun (type="Integer")
+  * @ORM\Column (type="integer")
   * @ORM\GeneratedValue(strategy="AUTO")
   */
-
   protected $id;
 
   public function __construct()
   {
-    parent::_construct();
+    parent::__construct();
   }
 
 }
