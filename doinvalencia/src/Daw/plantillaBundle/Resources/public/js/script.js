3bb26ba0445f50550b1287b31c3151b578add5c1
Victor <victorvte94@gmail.com>
2016-02-22 10:09:43 +0000

    retocado el footer

--- doinvalencia/src/Daw/plantillaBundle/Resources/public/js/script.js
+++ doinvalencia/src/Daw/plantillaBundle/Resources/public/js/script.js
@@ -1,3 +1,11 @@
 function abrirComentarios(){
     $('#comentarios').toggle(1000);
 }
+
+$(document).ready(function(){
+
+	$("#categoriasNavBar").click(function(){
+	    $("#efectoNavBar").toggle(300);
+	});
+
+});
